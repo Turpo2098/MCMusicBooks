2ab183b0d4bbd01f7c061b2c3f5d60ac53693da0
Turpo2098 <thilo2002@gmx.de>
2024-01-11 19:15:02 +0100

    Fixed Octaves

--- src/main/java/tf/tfischer/musicbooks/interpreter/LazyLexer.java
+++ src/main/java/tf/tfischer/musicbooks/interpreter/LazyLexer.java
@@ -124,11 +124,11 @@ public class LazyLexer {
             }
             case "A","B","C","D","E","F","G" -> {
                 Note.Tone tone = Note.Tone.valueOf(str);
-                return Optional.of(new Token(TokenType.NOTE, tone,2));
+                return Optional.of(new Token(TokenType.NOTE, tone,1));
             }
             case "a","b","c","d","e","f","g" -> {
                 Note.Tone tone = Note.Tone.valueOf(str.toUpperCase());
-                return Optional.of(new Token(TokenType.NOTE, tone,1));
+                return Optional.of(new Token(TokenType.NOTE, tone,0));
             }
 
             //Instruments
