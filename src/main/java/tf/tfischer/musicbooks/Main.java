7131ccf7be2b15fc0edae3d5008bbd2db58759b3
Turpo2098 <49616465+Turpo2098@users.noreply.github.com>
2024-01-12 12:20:32 +0100

    Removed Warnings

--- src/main/java/tf/tfischer/musicbooks/Main.java
+++ src/main/java/tf/tfischer/musicbooks/Main.java
@@ -3,11 +3,13 @@ package tf.tfischer.musicbooks;
 import org.bukkit.plugin.java.JavaPlugin;
 import tf.tfischer.musicbooks.interpreter.commands.PlayCommand;
 
+import java.util.Objects;
+
 public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getCommand("music").setExecutor(new PlayCommand(getServer()));
+        Objects.requireNonNull(getCommand("music")).setExecutor(new PlayCommand(getServer()));
 
     }
 
